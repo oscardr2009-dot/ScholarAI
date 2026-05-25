b6319dc7ba86ba1b967eb6e4b2dadebe911c5fae
Oscar Rodriguez <oscardr2009@gmail.com>
2026-05-25 12:38:52 -0400

    Create firebase.js

---
+++ src/firebase.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyByf6R9Y4Dd0xfJHA9QKlOLmqOPRFgLH2c",
+  authDomain: "apexhighai.firebaseapp.com",
+  projectId: "apexhighai",
+  storageBucket: "apexhighai.firebasestorage.app",
+  messagingSenderId: "366716475228",
+  appId: "1:366716475228:web:bae381e73a1d8db8d49399"
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
